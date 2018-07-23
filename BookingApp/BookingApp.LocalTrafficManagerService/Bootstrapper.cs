94bf960929fa536495f32b8216f152d4ec0cc689
Dzmitry Krupnou <Dzmitry_Krupnou@epam.com>
2018-07-23 23:01:51 +0300

    ApiGateway service skeleton was added; Default nancy's status code handlers were removed from ltm and registry services

--- BookingApp/BookingApp.LocalTrafficManagerService/Bootstrapper.cs
+++ BookingApp/BookingApp.LocalTrafficManagerService/Bootstrapper.cs
@@ -2,12 +2,18 @@
 using BookingApp.LocalTrafficManagerService.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.TinyIoc;
 
 namespace BookingApp.LocalTrafficManagerService
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override
+            Func<ITypeCatalog, NancyInternalConfiguration> InternalConfiguration =>
+            NancyInternalConfiguration
+                .WithOverrides(builder => builder.StatusCodeHandlers.Clear());
+
         private readonly IServiceProvider _serviceProvider;
 
         public Bootstrapper(IServiceProvider serviceProvider)
---
+++ BookingApp/BookingApp.RegistryService/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nancy;
+using Nancy.Bootstrapper;
+
+namespace BookingApp.RegistryService
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override
+            Func<ITypeCatalog, NancyInternalConfiguration> InternalConfiguration =>
+            NancyInternalConfiguration
+                .WithOverrides(builder => builder.StatusCodeHandlers.Clear());
+    }
+}
