56f20f580e1fda363d5f1b6e7eaad69cd2274d58
dkrupnou <dzmitry.krupnou@gmail.com>
2018-07-26 16:38:20 +0300

    Basic web api preparation were applied

---
+++ BookingApp/BookingApp.BookingService/ServiceLayer/BookingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApp.BookingService.ServiceLayer
+{
+    [Produces("application/json")]
+    [Route("booking")]
+    public class BookingController : Controller
+    {
+        public BookingController()
+        {  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
